d26720778ba5774e210343085ea1b5be5b4ee8ac
pgpump <pg079898@gmail.com>
2024-11-25 07:14:41 +0700

    Update adfolatops.js

--- ad/adfolatops.js
+++ ad/adfolatops.js
@@ -1,4 +1,4 @@
-document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.1;">');
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01;">');
 document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
 
 atOptions = {
