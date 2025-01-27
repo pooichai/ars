59aa96196ea24c04e00f2887643cad14542828d8
pgpump <pg079898@gmail.com>
2025-01-27 19:44:13 +0700

    Update button.js

--- float/button.js
+++ float/button.js
@@ -1,17 +1,12 @@
 document.write(`
-<div id="floatads" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:9999">
-  <div><a id="close-floatads" onclick="document.getElementById('floatads').style.display='none';" style="cursor:pointer;"><img alt="close" src="https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif" title="close button"/></a></div>
-  <div style="text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto">
-    <script>
-    atOptions = {
-      'key': 'fa36fa4c82dc8022fda12fbc52902556',
-      'format': 'iframe', 
-      'height': 90,
-      'width': 728,
-      'params': {}
-    };
-    </script>
-    <script type="text/javascript" src="//www.topcreativeformat.com/fa36fa4c82dc8022fda12fbc52902556/invoke.js"></script>
+<div id='floatads' style='width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999'>
+  <div>
+    <a id='close-floatads' onclick='document.getElementById("floatads").style.display = "none";' style='cursor:pointer;'>
+      <img alt='close' src='https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif' title='close button'/>
+    </a>
+  </div>
+  <div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
+    <script src="https://pgpump.github.io/float/folat.js" type="text/javascript"><\/script>
   </div>
 </div>
 `);
