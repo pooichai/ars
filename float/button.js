1160862244e090d970ec55cf8f198b9851a3b049
pgpump <pg079898@gmail.com>
2025-01-27 19:29:07 +0700

    Create button.js

---
+++ float/button.js
@@ -0,0 +1,15 @@
+<div id='floatads' style='width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999'>
+<div><a id='close-floatads' onclick='document.getElementById(&apos;floatads&apos;).style.display = &apos;none&apos;;' style='cursor:pointer;'><img alt='close' src='https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif' title='close button'/></a></div>
+<div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
+
+atOptions = {
+		'key' : 'fa36fa4c82dc8022fda12fbc52902556',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/fa36fa4c82dc8022fda12fbc52902556/invoke.js"></scr' + 'ipt>');
+
+</div>
+</div>
