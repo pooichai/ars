73e2d643be73c5551285a3699aee3bb1e16000d2
pgpump <pg079898@gmail.com>
2025-01-28 03:41:29 +0700

    Update button.js

--- float/button.js
+++ float/button.js
@@ -2,7 +2,7 @@ document.write(`
 <div id='floatads' style='width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999'>
   <div>
     <a id='close-floatads' onclick='document.getElementById("floatads").style.display = "none";' style='cursor:pointer;'>
-      <img alt='close' src='https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif' title='close button'/>
+      <img src='https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif'/>
     </a>
   </div>
   <div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
