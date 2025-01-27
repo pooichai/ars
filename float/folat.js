6a816ebcf4d50744e13f2828e1da483797a3dc9a
pgpump <pg079898@gmail.com>
2025-01-27 19:44:45 +0700

    Create folat.js

---
+++ float/folat.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'fa36fa4c82dc8022fda12fbc52902556',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/fa36fa4c82dc8022fda12fbc52902556/invoke.js"></scr' + 'ipt>');
