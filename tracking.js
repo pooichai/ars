2ad231d5775b3a2257b4dfd2b77ae6d06814f7e7
pooichai <pooichai7@gmail.com>
2025-04-08 02:53:20 +0700

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,16 @@
+///// Histats /////
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4909422&101"width="0" height="0"/>');
+
+///// Statcounter /////
+var sc_project=12978198; 
+var sc_invisible=1; 
+var sc_security="9f31a33b"; 
+
+document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
+document.write('<noscript>');
+document.write('<div class="statcounter">');
+document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/12978198/0/9f31a33b/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('</a>');
+document.write('</div>');
+document.write('</noscript>');
