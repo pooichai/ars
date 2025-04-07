73df2024205e112fe563e9ab83aeee7046e0d5c1
pooichai <pooichai7@gmail.com>
2025-04-08 06:13:34 +0700

    Update body.js

--- body.js
+++ body.js
@@ -1,5 +1,5 @@
-document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
-document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
 
 //Paste Iklan Disini
 
