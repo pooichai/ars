1ca9e13c3b9d73ee676dd5576d82fd580bef86db
pooichai <pooichai7@gmail.com>
2025-04-08 03:53:02 +0700

    Create body.js

---
+++ body.js
@@ -0,0 +1,18 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+//Paste Iklan Disini
+
+atOptions = {
+    'key' : 'ea7073a5f88001ce3ea7a9725829313d',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/ea7073a5f88001ce3ea7a9725829313d/invoke.js"></scr' + 'ipt>');
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
