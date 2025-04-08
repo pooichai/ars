e5e31b7fdc281549eec43f94c0a8a57969ec6b12
pooichai <pooichai7@gmail.com>
2025-04-08 07:33:00 +0700

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,16 +1,17 @@
-///// Histats /////
-document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4909422&101"width="0" height="0"/>');
+//Histats
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4908480&101"/>');
 
 ///// Statcounter /////
-var sc_project=12978198; 
+
+var sc_project=13059466; 
 var sc_invisible=1; 
-var sc_security="9f31a33b"; 
+var sc_security="c801499b"; 
 
 document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
 document.write('<noscript>');
 document.write('<div class="statcounter">');
 document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
-document.write('<img class="statcounter" src="https://c.statcounter.com/12978198/0/9f31a33b/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/13059466/0/c801499b/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
 document.write('</a>');
 document.write('</div>');
 document.write('</noscript>');
