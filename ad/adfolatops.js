be89bfd9790af5fee1f746e75a19febfc676f362
pooichai <pooichai7@gmail.com>
2025-06-12 05:37:59 +0700

    Update adfolatops.js

--- ad/adfolatops.js
+++ ad/adfolatops.js
@@ -1,1 +1,14 @@
-document.write('<scr' + 'ipt type="text/javascript" src="//pl23882093.profitableratecpm.com/af/9a/8a/af9a8ae00069b05d833307fdcd92df80.js"></scr' + 'ipt>');
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
+
+atOptions = {
+    'key': 'ea7073a5f88001ce3ea7a9725829313d',
+    'format': 'iframe',
+    'height': 90,
+    'width': 728,
+    'params': {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/ea7073a5f88001ce3ea7a9725829313d/invoke.js"></scr' + 'ipt>');
+
+document.write('</div>');
+document.write('</div>');
