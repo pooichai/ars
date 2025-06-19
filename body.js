3724df86a411a1dfcbb86f00a1bab0e8706a1e1e
pooichai <pooichai7@gmail.com>
2025-06-20 06:49:39 +0700

    Update body.js

--- body.js
+++ body.js
@@ -1,8 +1,2 @@
-document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01">');
-document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
-
-
-document.write('<scr' + 'ipt data-cfasync="false" src="//dcbbwymp1bhlf.cloudfront.net/?wbbcd=1194062"></scr' + 'ipt>');
-
-document.write('</div>');
-document.write('</div>');
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1712168976408933"
+     crossorigin="anonymous"></script>
