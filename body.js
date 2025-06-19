3f82119705b1cf4d2b7c4ffb52a7ebeff484ae12
pooichai <pooichai7@gmail.com>
2025-06-20 03:19:27 +0700

    Update body.js

--- body.js
+++ body.js
@@ -1,21 +1,8 @@
 document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01">');
 document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
 
-//Paste Iklan Disini
 
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1712168976408933"
-     crossorigin="anonymous"></script>
-<!-- respon -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1712168976408933"
-     data-ad-slot="6953885885"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-// Batas Iklan
+document.write('<scr' + 'ipt data-cfasync="false" src="//dcbbwymp1bhlf.cloudfront.net/?wbbcd=1194062"></scr' + 'ipt>');
 
 document.write('</div>');
 document.write('</div>');
