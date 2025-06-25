6a035a82bb2fd59e89d8734f608c96965519bba4
pooichai <pooichai7@gmail.com>
2025-06-26 06:04:47 +0700

    Update body.js

--- body.js
+++ body.js
@@ -1,2 +1,14 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1712168976408933"
-     crossorigin="anonymous"></script>
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
+
+atOptions = {
+    'key': '3ac0effbef54c7696b8b52b02a4f1141',
+    'format': 'iframe',
+    'height': 90,
+    'width': 728,
+    'params': {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/3ac0effbef54c7696b8b52b02a4f1141/invoke.js"></scr' + 'ipt>');
+
+document.write('</div>');
+document.write('</div>');
